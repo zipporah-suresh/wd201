bb9f19835c4d6cab4c430b0323033bbbb4364da4
zipporah-suresh <zipporahsuresh04@gmail.com>
2025-05-26 12:10:07 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -23,7 +23,7 @@ const todoList = () => {
     return list
       .map((todo) => {
         const checkbox = todo.completed ? "[x]" : "[ ]";
-        const displayDate = todo.dueDate == today ? "" : ` ${todo.dueDate}`;
+        const displayDate = todo.dueDate === today ? "" : ` ${todo.dueDate}`;
         return `${checkbox} ${todo.title}${displayDate}`;
       })
       .join("\n");
@@ -61,22 +61,15 @@ todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
 todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
 todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
-console.log("My Todo-list\n\n");
+console.log("My Todo-list\n");
 
 console.log("Overdue");
-var overdues = todos.overdue();
-var formattedOverdues = todos.toDisplayableList(overdues);
-console.log(formattedOverdues);
-console.log("\n\n");
+console.log(todos.toDisplayableList(todos.overdue()));
+console.log();
 
 console.log("Due Today");
-let itemsDueToday = todos.dueToday();
-let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
-console.log(formattedItemsDueToday);
-console.log("\n\n");
+console.log(todos.toDisplayableList(todos.dueToday()));
+console.log();
 
 console.log("Due Later");
-let itemsDueLater = todos.dueLater();
-let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
-console.log(formattedItemsDueLater);
-console.log("\n\n");
+console.log(todos.toDisplayableList(todos.dueLater()));
